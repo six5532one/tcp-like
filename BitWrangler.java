74cf656fccddc7a829f33d4c3ad384e741a6ca14
Emily Chen <emchennyc@gmail.com>
2016-04-14 01:08:50 -0400

    modularized BitWrangler

--- BitWrangler.java
+++ BitWrangler.java
@@ -10,14 +10,6 @@ class BitWrangler  {
         return  ByteBuffer.allocate(4).putInt(value).array();
     }
     
-    /*
-    public static byte[] toByteArray(int value) {
-        return new byte[] {(byte)(value >> 24),
-                            (byte)(value >> 16),
-                            (byte)(value >> 8),
-                            (byte)value };
-    }
-    */
     public static byte[] toByteArray(BitSet bits) {
         byte[] bytes = new byte[(bits.length() + 7) / 8];
         for (int i=0; i<bits.length(); i++) {
@@ -41,22 +33,31 @@ class BitWrangler  {
         byte[] bytes = BitWrangler.toByteArray(bits);
         return bytes;
     }
+    
+    public static int toInt(byte[] bytes)   {
+        if (bytes.length == 4)
+            return BitWrangler.fromByteArray(bytes);
+        else { 
+            byte[] paddedBytes = {BitWrangler.toByteArray(0)[0],
+                                    BitWrangler.toByteArray(0)[0],
+                                    bytes[0],
+                                    bytes[1]};
+            return BitWrangler.fromByteArray(paddedBytes);
+        }
+    }
 
     public static void main(String[] args)  {
         byte[] bytes = BitWrangler.toByteArray(6000, 2); 
+        /*
         byte b1 = bytes[0]; 
         byte b2 = bytes[1];
-        /*
         String s1 = String.format("%8s", Integer.toBinaryString(b1 & 0xFF)).replace(' ', '0');
         String s2 = String.format("%8s", Integer.toBinaryString(b2 & 0xFF)).replace(' ', '0');
         System.out.print(s1);
         System.out.print(s2);
         */
         // convert byte[] to int
-        byte[] paddedBytes = {BitWrangler.toByteArray(0)[0],
-                                BitWrangler.toByteArray(0)[0],
-                                bytes[0],
-                                bytes[1]};
-        System.out.println(BitWrangler.fromByteArray(paddedBytes));
+        int intVal = BitWrangler.toInt(bytes);
+        System.out.println(intVal);
     }
 }
